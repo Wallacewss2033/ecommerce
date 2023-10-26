485471253d51b441b9ebb3d4a30601ebee8399eb
Wallacewss2033 <wallace-2033@hotmail.com>
2023-10-26 18:01:14 -0300

    first commit

---
+++ app/Enums/OrderStatusEnum.php
@@ -0,0 +1,37 @@
+<?php 
+
+namespace App\Enums;
+
+enum OrderStatusEnum: int {
+
+    case CART = 1;
+    case PENDING = 2;
+    case PAID = 3;
+    case CANCELED = 4;
+    case REJECTED = 5;
+
+    public function getName(): string 
+    {
+        return match ($this) 
+        {
+            self::CART => 'Criado',
+            self::PENDING => 'Pendente',
+            self::PAID => 'Pago',
+            self::CANCELED => 'Cancelado',
+            self::REJECTED => 'Não aprovado ',
+            default => 'Status não encontrado'
+        };
+    }
+
+    public function getStyle(): string {
+        return match ($this) 
+        {
+            self::CART => 'Criado',
+            self::PENDING => 'Pendente',
+            self::PAID => 'Pago',
+            self::CANCELED => 'Cancelado',
+            self::REJECTED => 'Não aprovado ',
+            default => 'Status não encontrado'
+        };
+    }
+}
