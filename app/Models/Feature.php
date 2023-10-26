90d5493b5c6dafe4e1470dc47deeae2912227ce8
Wallacewss2033 <wallace-2033@hotmail.com>
2023-10-26 18:26:02 -0300

    Modelagem: Model

---
+++ app/Models/Feature.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Feature extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'unit'
+    ];
+
+    public function skus(): BelongsToMany
+    {
+        return $this->belongsToMany(related: Sku::class)->using(FeatureSku::class);
+    }
+}
